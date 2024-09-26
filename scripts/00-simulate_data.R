158ba1a5fc525d58b1ad4030eb9355c47ab80232
thk.421 <tinak.kim@mail.utoronto.ca>
2024-09-26 11:54:25 -0400

    Fixed the purpose of this data simulation and cleaned the file to include only necessary libraries.

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
-# Purpose: Simulate data for property crime reports in Toronto over the past
-# 10 years, and make a graph showing whether or not the report was cleared.
+# Purpose: Simulate data for property crime reports in Toronto before and after
+# the onset of COVID-19, and indicate whether or not the report was cleared.
 # Author: Tina Kim
-# Date: 19 September 2024
+# Date: 26 September 2024
 # Contact: tinak.kim@mail.utoronto.ca
 # License: University of Toronto
 # Pre-requisites: Know where to get Toronto property crime data.
@@ -11,11 +11,6 @@
 
 #### Workspace setup ####
 
-#install.packages("tidyverse")
-#install.packages("janitor")
-#install.packages("knitr")
-#install.packages("opendatatoronto")
-
 library(tidyverse)
 library(janitor)
 library(knitr)
@@ -23,22 +18,24 @@ library(opendatatoronto)
 
 #### Simulate data ####
 
+# Set a random seed for random data simulations
 set.seed(123)
 
-# define parameters
+# Define parameters year, division, and crime
 
-# Use 2014 through 2024 to represent the past 10 years
-year <- 2014:2024
+# Use 2016 through 2023 to represent years before and after onset of COVID-19
+year <- 2016:2023
 # Represent each of the divisions in Toronto
 division <- c("D11", "D12", "D13", "D14", "D22", "D23", "D31", "D32", 
               "D33", "D41", "D42", "D43", "D51", "D52", "D53", "D55")
-# Represent property crime types
-crime <- c("Break and Enter", "Theft", "Fraud")
+# Represent possible crime types 
+crime <- c("Break and Enter", "Theft", "Fraud", "Assault", "Sexual Assault",
+           "Attempted Murder", "Other")
 
-
+# Randomly simulate 500 crime reports in random divisions in random years,
+# then whether or not the crime report was solved
 simulated_crime_data <-
   tibble(
-    # Randomly simulate 500 crime reports in random divisions in random years
     report_year = sample(year, size = 500, replace = TRUE),
     division = sample(division, size = 500, replace = TRUE),
     crime_type = sample(crime, size = 500, replace = TRUE),
