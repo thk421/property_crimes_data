8f83eca312a1bd39eadb22083197995d48f7e0bd
thk.421 <tinak.kim@mail.utoronto.ca>
2024-09-26 23:02:55 -0400

    Updated code format using lintr.

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -17,9 +17,9 @@ raw_data <- read_csv("data/raw_data/raw_crime_data.csv")
 cleaned_data <- raw_data %>%
   # Remove rows with missing variables
   na.omit(raw_data) |>
-  # Remove unnecessary variable 'CATEGORY' 
+  # Remove unnecessary variable 'CATEGORY'
   mutate(CATEGORY = NULL) |>
-  # Rename variables for better understanding 
+  # Rename variables for better understanding
   rename(year = REPORT_YEAR) |>
   rename(division = DIVISION) |>
   rename(crime = SUBTYPE) |>
@@ -27,16 +27,17 @@ cleaned_data <- raw_data %>%
   rename(count_cleared = COUNT_CLEARED) |>
   # Filter report years to 2016-2023
   filter(year >= 2016 & year <= 2023) |>
-  # Combine different Break & Enter property types into a single Home Invasion crime category
-  mutate(crime = if_else(str_detect(crime, "Break & Enter"), 
-                              "Home Invasion", crime)) |>
+  # Combine different Break & Enter property types into a single
+  # Home Invasion crime category
+  mutate(crime = if_else(str_detect(crime, "Break & Enter"),
+                         "Home Invasion", crime)) |>
   # Combine different Theft prices into a single Property Theft crime category
-  mutate(crime = if_else(str_detect(crime, "Theft"), 
-                              "Property Theft", crime)) |>
-  # Make a new column called 'covid_period' to indicate if the report was before or after 2020
-  mutate(covid_period = ifelse(year < 2020, 'Pre-COVID', 'Post-COVID'))
+  mutate(crime = if_else(str_detect(crime, "Theft"),
+                         "Property Theft", crime)) |>
+  # Make a new column called 'covid_period' to indicate if the
+  # report was before or after 2020
+  mutate(covid_period = ifelse(year < 2020, "Pre-COVID", "Post-COVID"))
 
 
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
-
